ad1f6723e454d311defb158091115cd913d1fd8c
fopor <foporgit@gmail.com>
2017-06-09 19:34:22 -0300

    End_game_logic_demo

--- js/audio_control.js
+++ js/audio_control.js
@@ -2,5 +2,24 @@
 //if the game calls for an audio, this script waits for the previous sounds 
 //to end before starting a new one
 
+//loads all the sound files
 var sadness_audio = new Audio('sounds/sadness.wav');
-sadness_audio.play();
+var back_music = new Audio ('sounds/back_music.wav');
+
+
+
+//keeps the background music playing in loop
+back_music.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+back_music.play();
+
+//sound efeccts at the end of the game
+var audio_end_game = function (){
+    //stops the background music
+    back_music.pause();
+
+    //play sadness_audio
+    sadness_audio.play();
+}
